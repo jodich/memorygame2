6c24c0207eea4ddb1f21d9fd53fe70b774a42949
Jodi Choo <jodich@Jodis-MacBook-Pro.local>
2018-05-10 21:34:53 +0800

    created game logic 4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [ 
+	{
+		rank: "queen",
+		suite: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suite: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suite: "hearts",
+		cardImage: "images/king-of-hearts.png"		
+	},
+	{
+		rank: "king",
+		suite: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -10,11 +32,14 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suite)
+
 
 	if (cardsInPlay.length === 2) {
-		checkForMatch;
+		checkForMatch();
 	}; 
 };
 

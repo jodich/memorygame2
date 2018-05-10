7cbd8908525ba97238a611c61c98a149b1206e6b
Jodi Choo <jodich@Jodis-MacBook-Pro.local>
2018-05-11 00:30:40 +0800

    created game logic 5

--- js/main.js
+++ js/main.js
@@ -25,33 +25,36 @@ var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
 	} else {
-	console.log("Sorry, try again");	
+	alert("Sorry, try again");	
 	}	
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank)
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suite)
 
+	this.setAttribute('src', cards[cardId].cardImage)
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
+	cardsInPlay = [];
 	}; 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
 
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+}
+}		
 
-
-
-
-
-
-
-/* https://github.com/GA-WDI/
-fundamentals/blob/master/assignment-solutions.md */
+createBoard();
